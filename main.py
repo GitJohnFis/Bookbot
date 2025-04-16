e4b6ac5fc311315219741129ad233c20cf656ebe
GitJohnFis <jfisiiahi69@gmail.com>
2025-04-16 01:44:17 +0000

    add two books in directory including mobydick and prideandprejudice.txt

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from stats import (
     get_char_dict_sort_char_count,
 )
 def main():
-            path_to_book = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+            print("Usage: python3 main.py <path_to_book>")
+            sys.exit(1)
+            path_to_book = sys.argv[1]
+            # path_to_book = "books/frankenstein.txt"
             book_text = get_book_text(path_to_book)
             num_words = get_num_words(book_text)
             chars_sorted = get_character_count(book_text)
             num_chars_dict = get_char_dict_sort_char_count(char_character_count)
             print_report(path_to_book, num_words, num_chars_dict)
-            # print(f"{num_words} found in the document")
+            print(f"{num_words} found in the document")
             
             
 def get_book_text(path_to_file):
